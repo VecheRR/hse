6c52b4d4743fa519abf7f501001661543d3b651f
Vlad <37485334+VecheRR@users.noreply.github.com>
2020-06-03 17:29:05 +0300

    the value of currencies is taken online

--- Currency Converter/ViewController.swift
+++ Currency Converter/ViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import Foundation
+
+struct all : Decodable {
+    var rates: [String:Double]
+    var base: String
+    var date: String
+}
+
+var courses: Dictionary<String, Double> = Dictionary()
 
 class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
-    
-    let pickerData = ["Rubles", "Dollars", "Euros", "Pounds", "Hryvni"]
-    let moneySign = ["₽", "$", "€", "£", "₴"]
-    let course = [1.0, 74.0, 80.0, 86.0, 3.0]
     
     @IBOutlet weak var fromPicker: UIPickerView!
     
@@ -20,8 +25,35 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         fromPicker.delegate = self
         fromPicker.dataSource = self
         
+
+        let urlString = "https://api.exchangeratesapi.io/latest"
+        let url = URL(string: urlString)!
+
+        URLSession.shared.dataTask(with: url) { data, response, error in
+
+            guard let data = data, error == nil else { return }
+
+            let result = try? JSONDecoder().decode(all.self, from: data)
+            let ans = result!.rates
+            
+            for (key, value) in ans {
+                courses[key] = value
+            }
+            
+            courses["UER"] = 1
+            courses["BTC"] = 0.00012 // 03.06.2020 17:12
+            
+            // print(courses.keys.sorted())
+            
+
+        }.resume()
+        
         super.viewDidLoad()
     }
+    
+    var pickerData = ["AUD", "BGN", "BTC", "BRL", "CAD", "CHF", "CNY", "CZK", "DKK", "EUR", "GBP", "HKD", "HRK", "HUF", "IDR", "ILS", "INR", "ISK", "JPY", "KRW", "MXN", "MYR", "NOK", "NZD", "PHP", "PLN", "RON", "RUB", "SEK", "SGD", "THB", "TRY", "USD", "ZAR"]
+    
+    let moneySymbol = ["AUD":"$", "BGN":"leva", "BTC":"₿", "BRL":"R$", "CAD":"C$", "CHF":"CHF", "CNY":"元", "CZK":"Kč", "DKK":"kr", "EUR":"€", "GBP":"£", "HKD":"HK$", "HRK":"kn", "HUF":"Ft", "IDR":"Rp", "ILS":"₪", "INR":"₹", "ISK":"kr", "JPY":"¥", "KRW":"₩", "MXN":"Mex$", "MYR":"RM", "NOK":"kr", "NZD":"NZ$", "PHP":"₱", "PLN":"zł", "RON":"L", "RUB":"₽", "SEK":"kr", "SGD":"S$", "THB":"฿", "TRY":"₺", "USD":"US$", "ZAR":"R"]
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 2
@@ -40,25 +72,25 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     @IBAction func convertTapped(_ sender: UIButton) {
         guard let money = moneyTF.text else { return }
         
-        let type1 = fromPicker.selectedRow(inComponent: 0)
-        let type2 = fromPicker.selectedRow(inComponent: 1)
-        
-        // guard type1 != type2 else { return }
+        let type1 = pickerData[fromPicker.selectedRow(inComponent: 0)]
+        let type2 = pickerData[fromPicker.selectedRow(inComponent: 1)]
         
         let convertedMoney : Double
         
         if type1 == type2 {
             convertedMoney = Double(money)!
-        } else if type1 == 0 {
-            convertedMoney = round((Double(money)! / course[type2]) * 1000) / 1000
+        } else if type1 == "EUR" {
+            convertedMoney = round((Double(money)! * courses[type2]!) * 100000.0) / 100000.0
+            print(courses[type2]!)
+        } else if type2 == "EUR" {
+            convertedMoney = round((Double(money)! / courses[type1]!) * 100000.0) / 100000.0
+            //print(courses[type2]!)
         } else {
-            // First, convert to rubles, and then from rubles to another currency
+            let convertedToEurosMoney = Double(money)! / courses[type1]!
             
-            let convertedToRublesMoney = Double(money)! * course[type1]
-            
-            convertedMoney = round((convertedToRublesMoney / course[type2]) * 1000) / 1000
+            convertedMoney = round((convertedToEurosMoney * courses[type2]!) * 100000.0) / 100000.0
         }
-        label.text = "\(convertedMoney)\(moneySign[type2])"
+        label.text = "\(convertedMoney) \(moneySymbol[type2] ?? "")"
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
